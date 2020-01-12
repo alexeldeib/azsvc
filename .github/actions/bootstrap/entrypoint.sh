5036940068760126c0dcd3eec4998627231540fc
Ace Eldeib <alexeldeib@gmail.com>
2020-01-11 23:58:42 -0800

    fix: task usage

--- .github/actions/bootstrap/entrypoint.sh
+++ .github/actions/bootstrap/entrypoint.sh
@@ -3,5 +3,3 @@ set -eux
 
 mkdir -p ${GITHUB_WORKSPACE}/bin
 cp /out/bin/bootstrap ${GITHUB_WORKSPACE}/bin/bootstrap
-
-/out/bin/bootstrap -f ${GITHUB_WORKSPACE}/${FILE} --app ${AZURE_CLIENT_ID} --tenant ${AZURE_TENANT_ID} --key ${AZURE_CLIENT_SECRET}
