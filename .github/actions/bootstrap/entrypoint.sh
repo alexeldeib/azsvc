9dbc5eeba1d7b7da7af94ff50f8b3bfe0b09428f
Ace Eldeib <alexeldeib@gmail.com>
2020-01-11 23:29:16 -0800

    fix: entrypoint in bash

--- .github/actions/bootstrap/entrypoint.sh
+++ .github/actions/bootstrap/entrypoint.sh
@@ -3,3 +3,5 @@ set -eux
 
 mkdir -p ${GITHUB_WORKSPACE}/bin
 cp /out/bin/bootstrap ${GITHUB_WORKSPACE}/bin/bootstrap
+
+/out/bin/bootstrap -f ${FILE} --app ${AZURE_CLIENT_ID} --tenant ${AZURE_TENANT_ID} --key ${AZURE_CLIENT_SECRET}
