9aa44c450c37aef1d74f22679aecd6c8fc127389
Ace Eldeib <alexeldeib@gmail.com>
2020-01-11 23:39:12 -0800

    fix: spaces

--- .github/actions/bootstrap/entrypoint.sh
+++ .github/actions/bootstrap/entrypoint.sh
@@ -4,4 +4,4 @@ set -eux
 mkdir -p ${GITHUB_WORKSPACE}/bin
 cp /out/bin/bootstrap ${GITHUB_WORKSPACE}/bin/bootstrap
 
-/out/bin/bootstrap -f "${FILE}" --app "${AZURE_CLIENT_ID}"--tenant "${AZURE_TENANT_ID}"--key "${AZURE_CLIENT_SECRET}"
+/out/bin/bootstrap -f "${FILE}" --app "${AZURE_CLIENT_ID}" --tenant "${AZURE_TENANT_ID}" --key "${AZURE_CLIENT_SECRET}"
